c3eadb8819b328a32359b9c24c3ff5cd10d60dab
Tim Jarratt <tjarratt@gmail.com>
2013-09-21 11:56:56 -0700

    Switch to a sqlite backing store
    @andymoe pointed out that it was a little silly to use an XML
    backing store. I wonder why this is the default when you add
    core data to a project in xcode 4 for an OS X project.

--- TwIRCk/GLGManagedObjectContext.m
+++ TwIRCk/GLGManagedObjectContext.m
@@ -72,7 +72,7 @@
 
     NSURL *url = [applicationFilesDirectory URLByAppendingPathComponent:@"TwIRCk.storedata"];
     NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:mom];
-    if (![coordinator addPersistentStoreWithType:NSXMLStoreType configuration:nil URL:url options:nil error:&error]) {
+    if (![coordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:url options:nil error:&error]) {
         [[NSApplication sharedApplication] presentError:error];
         return nil;
     }
